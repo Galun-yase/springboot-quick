a67aed4418881a25aa33f132ea5461b7bbee84a7
rcn <534897552rqc@gmail.com>
2020-07-06 23:01:42 +0800

    新增crud 视图解析、国际化、登录拦截器

--- src/main/java/com/chengzi/config/MyMvcConfig.java
+++ src/main/java/com/chengzi/config/MyMvcConfig.java
@@ -1,6 +1,11 @@
 package com.chengzi.config;
 
+import com.chengzi.component.LoginHandlerInterceptor;
+import com.chengzi.component.MyLocaleResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -15,4 +20,33 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
         //浏览器发送/chengzi请求来到404页面（也是模板引擎解析）
         registry.addViewController("/chengzi").setViewName("404");
     }
+
+    @Bean
+    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
+        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
+            //注册视图解析器
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/index.html").setViewName("login");
+                registry.addViewController("/main.html").setViewName("dashboard");
+
+            }
+
+            //注册拦截器
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**").
+                        excludePathPatterns("/index.html","/","/user/login");
+            }
+        };
+
+        return  adapter;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
+    }
+
 }
